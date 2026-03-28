c0df198b216522f591fabe220d9ff20b3d0fc0b7
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-03-28 03:21:13 +0000

    added GetMapping for getting book detail using id

--- src/main/java/com/workshop/bookshelf/controller/BookController.java
+++ src/main/java/com/workshop/bookshelf/controller/BookController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.workshop.bookshelf.model.Book;
 import com.workshop.bookshelf.service.BookService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class BookController {
@@ -38,5 +41,19 @@ public class BookController {
         // 3. Return the view name (resolves to templates/books.html)
         return "books";
 
+    }
+
+    //Step 3: Book Detail Page
+    @GetMapping("/books/{id}") // {id} is a URL placeholder
+    public String bookDetail(@PathVariable Long id, Model model, HttpServletResponse response){ //Spring extracts "2" from /books/2
+        return bookService.findById(id)
+                            .map(book -> {
+                                model.addAttribute("book", book);
+                                return "book-detail"; //resolves to templates/book-detail.html
+                            })
+                            .orElseGet(() -> {
+                                response.setStatus(404);
+                                return "error/404"; // resolves to templates/error/404.html
+                            });
     }
 }
