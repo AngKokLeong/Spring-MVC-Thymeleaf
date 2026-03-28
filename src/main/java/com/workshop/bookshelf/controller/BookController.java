f1eb68592f195f94f0f4c3af21a501b45e5f1566
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2026-03-28 02:23:15 +0000

    added GetMapping and listBooks method for retrieving list of books

--- src/main/java/com/workshop/bookshelf/controller/BookController.java
+++ src/main/java/com/workshop/bookshelf/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.workshop.bookshelf.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.workshop.bookshelf.model.Book;
 import com.workshop.bookshelf.service.BookService;
 
 @Controller
@@ -17,5 +21,22 @@ public class BookController {
     @GetMapping("/")
     public String home(){
         return "index";
+    }
+
+    //Step 2: Book List
+    @GetMapping("/books")
+    public String listBooks(Model model){
+        
+        //1. Fetch data from the service layer
+        List<Book> allBooks = bookService.findAll();
+        
+
+        //2. Add it to the Model under the key "books"
+        //  The template accesses it with $(books)
+        model.addAttribute("books", allBooks);
+
+        // 3. Return the view name (resolves to templates/books.html)
+        return "books";
+
     }
 }
